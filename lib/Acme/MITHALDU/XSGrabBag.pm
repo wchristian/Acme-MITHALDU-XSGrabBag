8639a979beba4cf98a08a698abf6b429478e9374
Christian Walde <walde.christian@googlemail.com>
2015-02-20 13:40:34 +0100

    implement mix and deg2rad

--- lib/Acme/MITHALDU/XSGrabBag.pm
+++ lib/Acme/MITHALDU/XSGrabBag.pm
@@ -1,11 +1,61 @@
-use strictures;
-
 package Acme::MITHALDU::XSGrabBag;
+use strictures;
+use Exporter 'import';
+use Acme::MITHALDU::XSGrabBag::Inline ();
 
 # VERSION
 
-# ABSTRACT:
+# ABSTRACT: a bunch of XS math functions i'm not sure where to shove yet
 
 # COPYRIGHT
+
+our @EXPORT_OK = qw( mix deg2rad );
+
+sub _func { Acme::MITHALDU::XSGrabBag::Inline->import( C => @_ ) }
+
+=head1 DESCRIPTION
+
+This module is an experimental space for me to work with XS functions meant for
+L<Microidium|https://github.com/wchristian/Microidium>.
+
+=head1 FUNCTIONS
+
+=head2 my $hash = mix( $a, $b, $c )
+
+Takes 3 x 32 bits of data as integers, mixes them, combining all 96 bits of
+input to generate a pseudo-random hash returned as a 32 bit integer.
+
+Original implementation by
+L<Bob Jenkins|http://web.archive.org/web/20071224045401/http://www.burtleburtle.net/bob/c/lookup2.c>.
+
+=cut
+
+_func <<'...';
+int mix(int a, int b, int c) {
+    a -= b; a -= c; a ^= (c>>13);
+    b -= c; b -= a; b ^= (a<<8);
+    c -= a; c -= b; c ^= (b>>13);
+    a -= b; a -= c; a ^= (c>>12);
+    b -= c; b -= a; b ^= (a<<16);
+    c -= a; c -= b; c ^= (b>>5);
+    a -= b; a -= c; a ^= (c>>3);
+    b -= c; b -= a; b ^= (a<<10);
+    c -= a; c -= b; c ^= (b>>15);
+    return c;
+}
+...
+
+
+=head2 my $deg = deg2rad( $rad )
+
+32 bit degree to radian conversion, dirty, but fast.
+
+=cut
+
+_func <<'...';
+float deg2rad(float degrees) {
+    return 0.0174532925 * degrees;
+}
+...
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -1,17 +1,34 @@
-use strictures;
-
 package basic_test;
-
+use strictures;
 use Test::InDistDir;
 use Test::More;
 
-use Acme::MITHALDU::XSGrabBag;
+use Acme::Math::XS qw' mix deg2rad ';
 
 run();
 done_testing;
 exit;
 
 sub run {
+
+    is mix( 1, 2, 1 ), -2046868284;
+    is mix( 1, 2, 1 ), -2046868284;
+    is mix( 1, 2, 1 ), -2046868284;
+    is mix( 2, 2, 1 ), -1026454904;
+    is mix( 3, 2, 1 ), -128124089;
+    is mix( 2, 2, 1 ), -1026454904;
+    is mix( 2, 2, 2 ), -1942914929;
+    is mix( 2, 2, 3 ), -642921038;
+    is mix( 2, 2, 4 ), -1206558357;
+    is mix( 3, 2, 1 ), -128124089;
+    is mix( 3, 2, 2 ), -1164826007;
+    is mix( 3, 2, 3 ), -1833207769;
+    is mix( 3, 2, 4 ), -766896240;
+
+    is deg2rad( 0 ),   0;
+    is deg2rad( 1 ),   0.0174532923847437;
+    is deg2rad( 180 ), 3.14159274101257;
+    is deg2rad( 360 ), 6.28318548202515;
 
     return;
 }
